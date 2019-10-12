c0ef200637737cc7d8cc9caedf4504c34ffed749
gewei <gewei@scapital.com>
2019-10-12 16:04:02 +0800

    借助标示位，优化冒泡

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -5,10 +5,16 @@ import java.util.Arrays;
 /**
  * 冒泡排序
  * 比较相邻的元素大小，进行交换
+ *
  * @author gewei
  * @date 2019/10/12
  */
 public class BubbleSort {
+    /**
+     * 冒泡排序
+     *
+     * @param as
+     */
     public void sort(int[] as) {
         int temp;
         for (int i = 0; i < as.length - 1; i++) {
@@ -22,12 +28,44 @@ public class BubbleSort {
         }
     }
 
+    /**
+     * 优化冒泡
+     *
+     * @param as
+     */
+    public void optimizeSort(int[] as) {
+        int i = 0;
+        int tmp;
+        for (i = 0; i < as.length - 1; i++) {
+            int j = 0;
+            int flag = 0;
+            for (j = 0; j < as.length - 1 - i; j++) {
+                if (as[j] > as[j + 1]) {
+                    tmp = as[j];
+                    as[j] = as[j + 1];
+                    as[j + 1] = tmp;
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                return;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         int[] arr = {3, 5, 4, 8, 7, 90, 80, 77};
 
         BubbleSort bubbleSort = new BubbleSort();
         bubbleSort.sort(arr);
         Arrays.stream(arr).forEach(i -> System.out.println(i));
+        System.out.println("优化后输出------");
+        int[] arr1 = {3, 5, 4, 8, 7, 90, 80, 77};
+        bubbleSort.sort(arr1);
+        Arrays.stream(arr).forEach(i -> System.out.println(i));
+
+
     }
 
 }
